f5d58ac87b7cc10ad280ed12d3d360e4abe0e8ca
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-03-11 22:40:20 +0000

    refactor stateless functional component into ed6 component as arrow function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Header(props){
+const Header = props => {
     return (
         <header className="top">
           <h1>Catch
@@ -13,23 +13,5 @@ function Header(props){
         </header>
     )
 }
-
-// class Header extends React.Component{
-//   render(){
-
-//     return(
-//       <header className="top">
-//         <h1>Catch
-//           <span className="ofThe"><span className="of">OF</span><span>THE</span></span>
-//           day
-//         </h1>
-//         <h3 className="tagline">
-//           <span>{this.props.tagline}</span>
-//         </h3>
-//       </header>
-//     )
-
-//   }
-// }
 
 export default Header;
