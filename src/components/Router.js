b5826b4d2d0342707a5252a2270c1dd66e766ac0
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-03-12 00:23:03 +0000

    update/install dependencies, add react router with route to root and render the storepicker component

---
+++ src/components/Router.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import StorePicker from "./StorePicker";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={StorePicker} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Router;
