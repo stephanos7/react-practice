f2d95bdf02c0c9de6d875d1d9e1568b76e04d756
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-03-12 21:42:46 +0000

    add additional route and catch-all 'not-found' route

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,12 +2,19 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import StorePicker from "./StorePicker";
+import App from "./App";
+import NotFound from "./NotFound";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
+        {/*index route takes an "exact path"*/}
         <Route exact path="/" component={StorePicker} />
+        {/*other routes take a path*/}
+        <Route path="/store/:id" component={App} />
+        {/*a catch-all route takes no paths*/}
+        <Route component={NotFound}/>
       </Switch>
     </BrowserRouter>
   )
