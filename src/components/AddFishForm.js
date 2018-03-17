fc2744ce2afef579ab3a8950d88425e4a381952b
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-03-17 14:58:56 +0000

    add component

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AddFishForm extends React.Component {
+  render(){
+    return(
+      <form>
+        <p>hello</p>
+      </form>
+    )
+  }
+
+}
+
+export default AddFishForm;
