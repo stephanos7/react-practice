84b195283fd40024816349ba464b5cc8ab527dea
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-03-17 15:20:32 +0000

    add form and addFish method as a property of the class to be called on submit

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,10 +1,23 @@
 import React from "react";
 
 class AddFishForm extends React.Component {
+
+  addFish = (e) => {
+    e.preventDefault();
+  }
+
   render(){
     return(
-      <form>
-        <p>hello</p>
+      <form className="fish-edit" onSubmit={this.addFish}> 
+        <input name="name" type="text" placeholder="name" />
+        <input name="price" type="text" placeholder="price" />
+        <select name="status" type="text">
+          <option value="available">fresh!</option>
+          <option vlaue="unavailable">sold out</option>
+        </select>
+        <textarea name="desc" placeholder="desc" />
+        <input name="image" type="text" placeholder="image" />
+        <button type="submit">Add Fish</button>
       </form>
     )
   }
