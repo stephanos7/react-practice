944a71d6322f03f7fa3fbb20e4c70aac4ebc38b0
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-03-17 14:52:22 +0000

    naming

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,12 +1,12 @@
 import React from "react";
-import addFishForm from "./addFishForm";
+import AddFishForm from "./AddFishForm";
 
 class Inventory extends React.Component{
   render(){
     return(
       <div className="inventory">
         <h2>Inventory</h2>
-        <addFishForm />
+        <AddFishForm />
       </div>
     )
   }
