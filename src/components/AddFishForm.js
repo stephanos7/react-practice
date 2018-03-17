623b123e6a606bae705d4431411de0978d219945
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-03-17 15:35:16 +0000

    capture form inputs via ref callbacks and add to fishDetails object

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -4,19 +4,27 @@ class AddFishForm extends React.Component {
 
   addFish = (e) => {
     e.preventDefault();
+    const fishDetails = {
+      name: this.fishName.value,
+      price: this.fishPrice.value,
+      status: this.fishStatus.value,
+      desc: this.fishDesc.value,
+      image:this.fishImage.value
+    }   
+    console.log(fishDetails)
   }
 
   render(){
     return(
       <form className="fish-edit" onSubmit={this.addFish}> 
-        <input name="name" type="text" placeholder="name" />
-        <input name="price" type="text" placeholder="price" />
-        <select name="status" type="text">
+        <input name="name" type="text" placeholder="name" ref={ input => this.fishName = input}/>
+        <input name="price" type="text" placeholder="price" ref={ input => this.fishPrice = input} />
+        <select name="status" type="text" ref={ input => this.fishStatus = input }>
           <option value="available">fresh!</option>
           <option vlaue="unavailable">sold out</option>
         </select>
-        <textarea name="desc" placeholder="desc" />
-        <input name="image" type="text" placeholder="image" />
+        <textarea name="desc" placeholder="desc"  ref={ input => this.fishDesc = input}/>
+        <input name="image" type="text" placeholder="image"  ref={ input => this.fishImage = input}/>
         <button type="submit">Add Fish</button>
       </form>
     )
